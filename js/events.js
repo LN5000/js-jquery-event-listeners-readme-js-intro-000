f63d50e3e775c21033c0129bfbe9c2e1295a9007
Lee <lee.o.noble@gmail.com>
2018-07-07 07:05:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,9 +2,14 @@
 
 $(document).ready(function(){
 getIt()
+<<<<<<< HEAD
 frameIt()
 pressIt()
 submitIt()
+=======
+// call functions here
+pressIt()
+>>>>>>> bf41b1ff6ee33f87935147847eadc684cd4ab48b
 });
 
 function getIt() {
@@ -14,12 +19,15 @@ function getIt() {
 });
 }
 
+<<<<<<< HEAD
 function frameIt() {
   $('img').on("load", function(){
     $("img").addClass("tasty");
 });
 }
 
+=======
+>>>>>>> bf41b1ff6ee33f87935147847eadc684cd4ab48b
 function pressIt() {
   $('form input#typing').on("keydown", function(key){
     if(key.which == 71){
@@ -29,8 +37,14 @@ function pressIt() {
 }
 
 function submitIt() {
+<<<<<<< HEAD
   $('form').on("submit", function(){
     alert(`Your form is going to be submitted now.`);
     //action you want taken
+=======
+  $('form input:submit').on("click", function(){
+    alert("Your form is going to be submitted now.");
+  }
+>>>>>>> bf41b1ff6ee33f87935147847eadc684cd4ab48b
 });
 }
